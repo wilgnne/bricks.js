b56e1df58b3faf2d3d65b708275f41d8b6bb88a9
Wilgnne Khawan <wilgnne.alencar@marttech.com.br>
2022-11-23 15:52:49 -0300

    With server

--- play/sketch.js
+++ play/sketch.js
@@ -1,4 +1,6 @@
-const socket = io("ws://localhost:3000");
+const nickname = prompt("Nickname:");
+
+const socket = io("ws://129.148.39.27:2001");
 
 let data = {
   heigth: 20,
@@ -24,6 +26,7 @@ const FRUIT_COLOR_TABLE = {
 socket.on("connect", () => {
   id = socket.id
   console.log(id)
+  socket.emit("register", nickname);
 })
 
 socket.on("tick", (d) => {
@@ -40,14 +43,22 @@ function drawWall() {
       const player =  data.players.find((npc) => npc.position.x === i && npc.position.y === j)
       if (player) {
         if (player.id === id)
-          fill('rgba(255, 255, 255, 0.50)');
-        else
           fill('rgba(255, 255, 255, 1)');
+        else
+          fill('rgba(255, 255, 255, 0.50)');
       }
-        
+
       rect(i * BLOCK_SIZE + i * 2 + 5, j * BLOCK_SIZE + j * 2 + 5, BLOCK_SIZE, BLOCK_SIZE);
     }
   }
+}
+
+function hank() {
+  data.players.sort((a, b) => b.score - a.score).forEach((player, index) => {
+    console.log(player)
+    fill('rgba(255, 255, 255, 1)');
+    text(`${index + 1} - ${player.name}: ${player.score}`, 200, 20 + (20 * index));
+  })
 }
 
 function keyPressed() {
@@ -66,9 +77,12 @@ function setup() {
   var canvas = createCanvas(400, 400);
   canvas.parent('screen');
   frameRate(5);
+  textFont("Arial");
+  textSize(20);
 }
 
 function draw() {
   clear();
+  hank();
   drawWall();
 }
